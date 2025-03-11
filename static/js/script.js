887069d5eff0a3dc4bdb95324fcb6a4e12de1cd6
Isaac J <88879000+fnfplayer64@users.noreply.github.com>
2025-03-11 00:55:24 +0000

    add index

--- static/js/script.js
+++ static/js/script.js
@@ -1,14 +1,24 @@
-player_name = prompt("enter your name");
-alert("Hello " + player_name);
-player_guess = prompt("lets play rock paper scissors");
-computer_guess = randominteger(1, 3);
-if (player_guess == computer_guess) {
-  document.getElementById("user_feedback").innerHTML =
-    "<em>Correct, you win</em>";
-} else {
-  document.getElementById("user_feedback").innerHTML = "incorrect, you lose";
-}
+gamerscorce = "1";
+computerscore = 1;
 
-function randominteger(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+sumscore = gamerscorce + computerscore;
+
+alert(sumscore);
+
+function mygame() {
+  player_name = prompt("enter your name");
+  alert("Hello " + player_name);
+  player_guess = prompt("Pick a number between 1 and 3");
+  computer_guess = randomInteger(1, 3);
+  if (player_guess == computer_guess) {
+    document.getElementById("user_feedback").innerHTML =
+      "<em>Correct, You WIN</em>";
+  } else {
+    document.getElementById("user_feedback").innerHTML =
+      "Incorrect, you LOSE, dummy\n" + "The computer guessed " + computer_guess;
+  }
+
+  function randomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
 }
